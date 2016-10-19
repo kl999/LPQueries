e9044b629ab3e52f81d4ac0f9826a5ce12f9cbe1
spa <a@mail.ru>
2016-10-19 09:49:29 +0600

    PS
    ADGroups

---
+++ ps/ADGroups.ps1
@@ -0,0 +1,12 @@
+﻿Get-ADGroup -Filter 'samAccountName -like "Paynet*"' |
+select SamAccountName, name
+
+$grName = "PaynetSystemAdministratorTerminal"
+
+$gr = (Get-ADGroup $grName)
+
+"
+----Group " + $grName + " members:
+"
+
+Get-ADGroupMember $gr | select SamAccountName, Name

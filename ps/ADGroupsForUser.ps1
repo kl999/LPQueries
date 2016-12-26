b1ad799cea37f3e74429cf15f10a2584c120932e
kl999 <klstr999@gmail.com>
2016-12-26 18:06:20 +0600

    AD all properties

--- ps/ADGroupsForUser.ps1
+++ ps/ADGroupsForUser.ps1
@@ -1,6 +1,6 @@
 ﻿#Get-ADUser samartsev_26224
 $u = Get-ADUser -Filter{Name -like "самарцев*"} `
-    -Properties Mail, DisplayName, TelephoneNumber
+    -Properties Mail, DisplayName, TelephoneNumber #*
 $u
 
 Get-ADPrincipalGroupMembership $u.SamAccountName |

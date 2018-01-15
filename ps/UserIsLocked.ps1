b24deb2d520068ee89fde64230bfaaaeb88a6483
kl999 <klstr999@gmail.com>
2018-01-15 14:46:26 +0600

    Is user locked

---
+++ ps/UserIsLocked.ps1
@@ -0,0 +1,16 @@
+﻿#Get-ADUser samartsev_26224
+[string[]] $srch = @("самарцев*")
+#$srch += "*Platon.Samartsev@kaspi.kz*"
+
+foreach($srchs in $srch) {
+$u = Get-ADUser -Filter{Name -like $srchs -or Mail -like $srchs} `
+    -Properties Mail, DisplayName, TelephoneNumber, `
+    Deleted, AccountExpirationDate, AccountLockoutTime, `
+    BadLogonCount, badPwdCount, CannotChangePassword, `
+    Enabled, isDeleted, LastBadPasswordAttempt, LockedOut, `
+    PasswordExpired, PasswordLastSet, PasswordNeverExpires, PasswordNotRequired #,*
+$u
+"
+------------------------------
+"
+}
